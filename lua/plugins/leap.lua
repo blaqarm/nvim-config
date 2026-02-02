5c3df562f2743363bd90ed0dbc3d0cbe2d142228
blaqarm <illillusions13@gmail.com>
2026-02-02 15:50:24 +0300

    add few changes

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,6 +2,15 @@ return {
 	"ggandor/leap.nvim",
 	lazy = false,
 	config = function()
-		require("leap").add_default_mappings(true)
+		-- старая тема
+		--require("leap").add_default_mappings(true)
+		require("leap")
+
+    -- прыжки
+    vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)",  { desc = "Leap forward" })
+    vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)", { desc = "Leap backward" })
+
+    -- прыжок между окнами
+    --vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)", { desc = "Leap from window" })
 	end,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,30 +1,41 @@
 return {
-	{
-		'neovim/nvim-lspconfig',
-		config = function()
-			local lspconfig = require('lspconfig')
-			lspconfig.lua_ls.setup{}
-			lspconfig.ts_ls.setup{}
-			lspconfig.html.setup{}
-			lspconfig.phpactor.setup{}
-			lspconfig.cssls.setup{}
-			vim.api.nvim_create_autocmd('LspAttach', {
-			    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-				callback = function(ev)
-					vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      -- регистрируем конфиги серверов
+      vim.lsp.config("lua_ls", {})
+      vim.lsp.config("ts_ls", {})
+      vim.lsp.config("html", {})
+      vim.lsp.config("phpactor", {})
+      vim.lsp.config("cssls", {})
 
-					local opts = {buffer = ev.buf}
-					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-					vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-					vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-				    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-					vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, opts)
-					vim.keymap.set('n', '<Leader>lr', vim.lsp.buf.rename, opts)
-					vim.keymap.set({'n', 'v'}, '<Leader>la', vim.lsp.buf.code_action, opts)
-					vim.keymap.set('n', '<Leader>lf',
-						function() vim.lsp.buf.format {async = true} end, opts)
-		end
-	})
-	end
-	}
+      -- включаем сервера (можно запускать при старте)
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("html")
+      vim.lsp.enable("phpactor")
+      vim.lsp.enable("cssls")
+
+      -- автокоманда с keymaps, как у тебя
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+          local opts = { buffer = ev.buf }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
+          vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, opts)
+          vim.keymap.set({ "n", "v" }, "<Leader>la", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<Leader>lf", function()
+            vim.lsp.buf.format { async = true }
+          end, opts)
+        end,
+      })
+    end,
+  },
 }
+
