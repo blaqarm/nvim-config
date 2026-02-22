ce0e09ae12f978ae05d2ae714470a849a6b0752e
blaqarm <illillusions13@gmail.com>
2026-02-22 23:38:42 +0300

    fix toggleterm for arch

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,25 +1,41 @@
 return {
-	{
-		'akinsho/toggleterm.nvim',
-		version = "*",
-		config = function()
-			require('toggleterm').setup({
-			open_mapping = [[<c-\>]],
-			shell = "C:\\Users\\blaQarm\\AppData\\Local\\Programs\\nu\\bin\\nu.exe",
-		    })
-		function _G.set_terminal_keymaps()
-		  local opts = {buffer = 0}
---		  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-		  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-		  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-		  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-		  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-		  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-		  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-		end
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
 
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-		end	
-	}
+    -- lazy будет загружать плагин по нажатию Ctrl+\
+    keys = {
+      { "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+    },
+
+    config = function()
+      vim.opt.shell = "/usr/bin/zsh"
+
+      require("toggleterm").setup({
+        open_mapping = [[<C-\\>]], 
+        direction = "horizontal", -- вроде в старом было horizontal а не float
+--        float_opts = {
+--          border = "rounded",
+--        },
+        start_in_insert = true,
+        insert_mappings = true,
+        terminal_mappings = true,
+        close_on_exit = true,
+      })
+
+      -- Маппинги только внутри toggleterm
+      vim.api.nvim_create_autocmd("TermOpen", {
+        pattern = "term://*toggleterm#*",
+        callback = function()
+          local opts = { buffer = 0, silent = true }
+          vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
+          vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+          vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+          vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+          vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+          vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+        end,
+      })
+    end,
+  },
 }
