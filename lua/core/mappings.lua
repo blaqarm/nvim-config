d09db7584e9a90a373287a3afaef7338e2dfb67e
blaqarm <illillusions13@gmail.com>
2026-08-20 16:22:38 +0300

    add fix mapping for markdown

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -84,30 +84,36 @@ vim.keymap.set("n", "gcA", function()
 	vim.api.nvim_win_set_cursor(0, { vim.fn.line("."), #vim.api.nvim_get_current_line() })
 	vim.cmd("startinsert!")
 end, { desc = "Comment at end of line" })
+--
+--
+-- В mappings.lua (или отдельном plugins/markdown.lua)
 
--- Быстрое переключение markdown-чекбокса: - [ ] <-> - [x]
 local function toggle_checkbox()
 	local line = vim.api.nvim_get_current_line()
 
-	-- Если строка содержит чекбокс
 	if line:match("^%s*[-*+] %[[ xX]%]") then
 		local new_line
 		if line:match("%[[xX]%]") then
-			new_line = line:gsub("%[[xX]%]", "[ ]", 1) -- снять галочку
+			new_line = line:gsub("%[[xX]%]", "[ ]", 1)
 		else
-			new_line = line:gsub("%[ %]", "[x]", 1) -- поставить галочку
+			new_line = line:gsub("%[ %]", "[x]", 1)
 		end
 		vim.api.nvim_set_current_line(new_line)
 	else
-		-- Если чекбокса нет — добавить его в начало списка
+		-- Добавить чекбокс, если его нет
 		local new_line = line:gsub("^(%s*[-*+] )", "%1[ ] ", 1)
 		if new_line == line then
-			-- Если это просто текст без маркера списка — сделать пунктом
 			new_line = "- [ ] " .. line
 		end
 		vim.api.nvim_set_current_line(new_line)
 	end
 end
 
-vim.keymap.set("n", "<leader>cit", toggle_checkbox, { desc = "Toggle checkbox" })
-vim.keymap.set("v", "<leader>cit", toggle_checkbox, { desc = "Toggle checkbox" })
+-- Только для markdown: cit = change todo (как в org-mode)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.keymap.set("n", "cit", toggle_checkbox, { buffer = true, desc = "Toggle checkbox" })
+		vim.keymap.set("v", "cit", toggle_checkbox, { buffer = true, desc = "Toggle checkbox" })
+	end,
+})
